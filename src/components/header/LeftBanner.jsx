fbfd3c1afe177611d5710ed6a88e01ad6f19a88b
Shahin <102747797+ShahinSabour@users.noreply.github.com>
2024-11-22 17:08:37 +0100

    Update LeftBanner.jsx

--- src/components/header/LeftBanner.jsx
+++ src/components/header/LeftBanner.jsx
@@ -8,7 +8,7 @@ export default function LeftBanner(){
             <div className="main_name">Hi, I'm <span>Shahin Sabour</span></div>
             <div className="main_position"><span>a</span> <span class="varied_text">Front-End Developer</span></div>
             <p>
-                Front-end developer with over 2 years of experience building responsive, user-centered web applications. Skilled in modern JavaScript frameworks, TypeScript, and styling libraries, I focus on creating seamless, intuitive interfaces. Experienced in leveraging build tools and Git version control to streamline and collaborate effectively, I bring a strong foundation in web development best practices to every project
+                Front-end developer with over four years of experience building responsive, user-centered web applications. Skilled in modern JavaScript frameworks, TypeScript, and styling libraries, I focus on creating seamless, intuitive interfaces. Experienced in leveraging build tools and Git version control to streamline and collaborate effectively, I bring a strong foundation in web development best practices to every project
             </p> 
         
             <div className="social">
