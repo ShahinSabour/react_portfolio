3afa6cf2b868a0cce75e7caa86a2bd0d7d81f375
Shahin <102747797+ShahinSabour@users.noreply.github.com>
2024-11-29 17:26:24 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container: {
+      padding: {
+        DEFAULT: '3rem',
+        sm: '2rem',
+        lg: '2rem',
+        xl: '3rem',
+        '2xl': '6rem',
+      }
+    }
   },
   plugins: [],
 }
